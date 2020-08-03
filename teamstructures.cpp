b94a709b093818e78372c3b98bfd8195429f4ea9
btramsyeehaw <51712687+btramsyeehaw@users.noreply.github.com>
2020-08-03 21:12:41 +0200

    Add files via upload
    file that takes names from a text file

--- teamstructures.cpp
+++ teamstructures.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct player {
-  char team [8];
+  //char team [8];
   int number;
   string name;
   string surname;
 };
 
 player input_data (player k){
-  cout<<"please input the data for the player"<<endl<<"team: ";
-  cin>> k.team;
-  cout<<endl<<"player's number: ";
-  cin>> k.number;
-  cout<<endl<<"player's name: ";
-  cin>> k.name;
-  cout<<endl<<"player's surname: ";
-  cin>> k.surname;
-  return k;
+          ifstream file_we( "SK.txt" );
+          file_we >> k.number>>k.name>>k.surname;
+          return k;
 }
 
 void output_data (player k){
-  cout<<k.team<<" | #"<<k.number<<" "<<k.name<<" "<<k.surname;
+  cout<<"SK"<<" | #"<<k.number<<" "<<k.name<<" "<<k.surname;
 }
 //struct VisitLineup
 
 int main() {
-  player k[3];
-  for (int i=0; i<3; i++){
+
+  player k[17];
+  for (int i=0; i<17; i++){
     k[i]=input_data(k[i]);
   }
 
-  for (int i=0; i<3; i++){
+  for (int i=0; i<17; i++){
     output_data(k[i]);
     cout<<endl;
   }
