c53a31606909212f7b73b229692f1f1cee33b10b
btramsyeehaw <51712687+btramsyeehaw@users.noreply.github.com>
2020-08-03 22:17:32 +0200

    Add files via upload
    updated to read one team from file

--- teamstructures.cpp
+++ teamstructures.cpp
@@ -1,38 +1,37 @@
+#include <fstream>
 #include <iostream>
 using namespace std;
 
-struct player {
-  char team [8];
-  int number;
-  string name;
-  string surname;
+struct Player {
+    int number;
+    string name;
+    string surname;
 };
 
-player input_data (player k){
-  cout<<"please input the data for the player"<<endl<<"team: ";
-  cin>> k.team;
-  cout<<endl<<"player's number: ";
-  cin>> k.number;
-  cout<<endl<<"player's name: ";
-  cin>> k.name;
-  cout<<endl<<"player's surname: ";
-  cin>> k.surname;
-  return k;
+void output_data(Player k) {
+    cout << "SK"
+         << " | #" << k.number << " " << k.name << " " << k.surname;
 }
 
-void output_data (player k){
-  cout<<k.team<<" | #"<<k.number<<" "<<k.name<<" "<<k.surname;
-}
-//struct VisitLineup
+struct Team {
+  Player k[28];
+  Team(){
+    ifstream input_file ("SK.txt");
+    Player player;
+    int i = 0;
+    while (input_file >> player.number >> player.name >> player.surname) {
+        k[i] = player;
+        i++;
+    }
+  }
+};
 
 int main() {
-  player k[3];
-  for (int i=0; i<3; i++){
-    k[i]=input_data(k[i]);
-  }
 
-  for (int i=0; i<3; i++){
-    output_data(k[i]);
-    cout<<endl;
-  }
+Team SK;
+
+    for (int i = 0; i < 28; i++) {
+        output_data(SK.k[i]);
+        cout << endl;
+    }
 }
