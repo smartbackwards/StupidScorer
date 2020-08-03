0e006066197e3c7fb5cb47486db5e43bb5d564f2
Bartek Pacia <barpac02@gmail.com>
2020-08-03 21:37:38 +0200

    fixed boi

--- teamstructures.cpp
+++ teamstructures.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
-struct player {
-  //char team [8];
-  int number;
-  string name;
-  string surname;
+struct Player {
+    // char team [8];
+    int number;
+    string name;
+    string surname;
 };
 
-player input_data (player k){
-          ifstream file_we( "SK.txt" );
-          file_we >> k.number>>k.name>>k.surname;
-          return k;
+void output_data(Player k) {
+    cout << "SK"
+         << " | #" << k.number << " " << k.name << " " << k.surname;
 }
-
-void output_data (player k){
-  cout<<"SK"<<" | #"<<k.number<<" "<<k.name<<" "<<k.surname;
-}
-//struct VisitLineup
+// struct VisitLineup
 
 int main() {
+    Player k[17];
 
-  player k[17];
-  for (int i=0; i<17; i++){
-    k[i]=input_data(k[i]);
-  }
+    ifstream input_file("SK.txt");
 
-  for (int i=0; i<17; i++){
-    output_data(k[i]);
-    cout<<endl;
-  }
+    Player player;
+    int i = 0;
+    while (input_file >> player.number >> player.name >> player.surname) {
+        k[i] = player;
+        i++;
+    }
+
+    for (int i = 0; i < 17; i++) {
+        output_data(k[i]);
+        cout << endl;
+    }
 }
